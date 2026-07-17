6881b7fd683500b652a5876650df25a47ae3d2df
刘骏霖 <763606865@qq.com>
2026-07-17 11:36:34 +0800

    fix: 1. 删除多余的migration

--- migrations/000007_conversation_key.down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE conversations
-DROP INDEX uk_app_conversation_key,
-DROP COLUMN conversation_key;
--- migrations/000007_conversation_key.up.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE conversations
-ADD COLUMN conversation_key VARCHAR(255) NULL COMMENT '业务侧传入的会话幂等键' AFTER conversation_no,
-ADD UNIQUE KEY uk_app_conversation_key (app_id, conversation_key);
